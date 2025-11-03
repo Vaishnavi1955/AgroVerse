d983a7868d1e850548b41ea9167ced4e9a326e52
Vaishnavi1955 <phadvaishnavi1905@gmail.com>
2025-11-03 23:13:26 +0530

    Project work

--- app.py
+++ app.py
@@ -6,16 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
 @app.route('/crop')
 def crop():
     return render_template('crop.html')
